781fb4c6bba8b3e6b038ac67dc5356bf7a8eed1c
DJ Carpenter <dcapt1990@gmail.com>
2020-04-30 16:16:30 -0700

    updating npm to yarn

---
+++ build/precache-manifest.549646664b4aae409b2c3f3acd4ac677.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea2fcc9021e0ce528c798464406b24c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efb3cc9ed3edb928aed5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a9ca05b9a30f3bf59166",
+    "url": "/static/js/2.278b51f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.278b51f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb3cc9ed3edb928aed5",
+    "url": "/static/js/main.c17becee.chunk.js"
+  },
+  {
+    "revision": "f780848bbcb2e237ac1c",
+    "url": "/static/js/runtime-main.a0bb7eea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
